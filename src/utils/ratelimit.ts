7d240f15d0e3e724b9b17feff804ee4d5e4033aa
MusseJusse <66657454+MusseJusse@users.noreply.github.com>
2024-11-15 17:06:20 +0000

    Update ratelimit to 15 in 10s

--- src/utils/ratelimit.ts
+++ src/utils/ratelimit.ts
@@ -4,7 +4,7 @@ import { Redis } from "@upstash/redis";
 // Create a new ratelimiter, that allows 5 requests per 10 seconds
 export const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(5, "10 s"),
+  limiter: Ratelimit.slidingWindow(15, "10 s"),
   analytics: true,
   prefix: "@upstash/ratelimit",
 });
