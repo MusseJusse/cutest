8580a3d462bbb19d2f745ecc18c91a4b1b28eb34
MusseJusse <mussejusse@gmail.com>
2024-11-19 18:20:10 +0000

    fixed Fallback import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { cookies } from "next/headers";
 import { Suspense } from "react";
-import { VoteFallback } from "~/components/ui/fallback";
+import { VoteFallback } from "~/components/ui/Fallback";
 import PokemonSprite from "~/components/ui/pokemon-sprite";
 import VoteButton from "~/components/ui/vote-button";
 import { getTwoPokemon, PokemonPair } from "~/sdk/pokemon";
